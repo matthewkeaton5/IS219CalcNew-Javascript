02cac2cb0e8adeb0f710dfecc5e10a8b4ae1abbf
gabrielasaboia <gs525@njit.edu>
2021-03-20 00:12:34 -0400

    Created Statistics class, constructor and mean function

--- src/models/Statistics.js
+++ src/models/Statistics.js
@@ -4,7 +4,10 @@ class Statistics {
         this.valueList = valueList;
     }
 
-    mean(valueList){
+    mean(valueList){//function that calculates the mean of a list of numerical data
         return math.mean(valueList);
+    }
+    median(){
+
     }
 }

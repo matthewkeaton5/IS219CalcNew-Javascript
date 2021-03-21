06200c5e4c43880689330c3614427364c6027504
gabrielasaboia <gs525@njit.edu>
2021-03-20 20:36:19 -0400

    Implementation of skewness

--- src/Operations/Skewness.js
+++ src/Operations/Skewness.js
@@ -1,8 +1,8 @@
-function Skewness(mean, std, sumOfAllData, numberOfDataPoints){
-    //for skewness we need the standard deviation (std), mean of the
-    //distribution, sum of all data points in the distribution, number of data points = amountOfDataPoints
-//looking for a skewness formula
-    return 0;
+import {sampleSkewness} from "simple-statistics";
+
+function Skewness(listOfValues){
+    //function that calculates skewness
+    return sampleSkewness(listOfValues);
 }
 
 module.exports = Skewness;

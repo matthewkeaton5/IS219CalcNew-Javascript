5c0cfd361c8b8f99acc20b4a467cf5917ec96840
gabrielasaboia <gs525@njit.edu>
2021-03-20 20:53:58 -0400

    Implementation of PopulationCorrelation.js

--- src/Operations/PopulationCorrelation.js
+++ src/Operations/PopulationCorrelation.js
@@ -1,5 +1,6 @@
-function PopulationCorrelation() {
-    return 0;
+import {sampleCorrelation} from "simple-statistics";
+function PopulationCorrelation(valueList1, valueList2) {
+    return sampleCorrelation(valueList1, valueList2);
 }
 
 module.exports = PopulationCorrelation;

6eed73c0490fb73620220946d59dfea23a90b974
GabrielaSaboia <gs525@njit.edu>
2021-03-22 20:40:15 -0400

    modifying the statistics model

--- src/models/Statistics.js
+++ src/models/Statistics.js
@@ -2,8 +2,9 @@ const Calculation = require('./Calculation');
 //require calculation.js to extend previously defined list of values class
 class Statistics extends Calculation.ListCalculations{
 
-    constructor(ListOfValues, op){//Constructor definition, design pattern specific to JavaScript
+    constructor(ListOfValues, listOfValues2, op){//Constructor definition, design pattern specific to JavaScript
         super(ListOfValues, op);//defining which constructor values are passed through inheritance
+        this.listOfvalues2 = listOfValues2;
     }
 }
 module.exports = Statistics;
